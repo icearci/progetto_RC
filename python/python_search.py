c952eff1e65a0afd3a825c51accf7c4b6a559278
bisi <bisazza.1762939@studenti.uniroma1.it>
2019-01-04 17:33:07 +0100

    risultati e bottoni con id progressivi

--- python/python_search.py
+++ python/python_search.py
@@ -67,10 +67,10 @@ def calcola_distanza(via1,citta1,provincia1,paese1,cap1,via2,citta2,provincia2,p
 
 def get_json_risultati(risultati,database):
 	json_dict = {}
-	print("[get_json_risultati] risultati = ")
-	print(risultati)
-	print("[get_json_risultati] database = ")
-	print(database)
+	# print("[get_json_risultati] risultati = ")
+	# print(risultati)
+	# print("[get_json_risultati] database = ")
+	# print(database)
 	cont = 0
 	for elem in risultati:
 		json_dict[str(cont)]=database.get(elem)
@@ -89,29 +89,29 @@ def algoritmo(database,info): #occorre ancora sortare alla fine
 			lista_tipo = []
 			#il primo ciclo filtra sul tipo stampante
 			for elem in lista_id:
-				print("[algoritmo] stampantetipoInfo = ",str(info.get("stampantetipo")),"stampantetipodatabase= ",database.get(elem)["stampantetipo"])
+				# print("[algoritmo] stampantetipoInfo = ",str(info.get("stampantetipo")),"stampantetipodatabase= ",database.get(elem)["stampantetipo"])
 
 				
 				if database.get(elem)["stampantetipo"]==info.get("stampantetipo"):
-					print("[algoritmo] match di tipo stampante trovato")
+					# print("[algoritmo] match di tipo stampante trovato")
 
 					lista_tipo.append(elem)
 			#il secondo ciclo filtra sul prezzo
 			for doc in lista_tipo:
-				print("[algoritmo] prezzoInfo = ",info.get("stampanteprezzo"),"prezzodatabase= ",database.get(doc)["stampanteprezzo"])
+				# print("[algoritmo] prezzoInfo = ",info.get("stampanteprezzo"),"prezzodatabase= ",database.get(doc)["stampanteprezzo"])
 				
 				if float(database.get(doc).get("stampanteprezzo")) <= float(info.get("stampanteprezzo")):
 					lista_prezzo.append(doc)
-					print("[algoritmo] appeso prezzo all lista_prezzo")
+					#print("[algoritmo] appeso prezzo all lista_prezzo")
 				
 			lista_serie = []
 			#il terzo sulle distanze(TO BE DONE)
 			for elem in lista_prezzo:
-				print("[algoritmo]entrato in ciclo calcola distanza")
+				#print("[algoritmo]entrato in ciclo calcola distanza")
 				doc = database.get(elem)
 				distanza = calcola_distanza(str(doc.get("varindirizzo")),str(doc.get("varcitta")),str(doc.get("varprovincia")),str(doc.get("varpaese")),str(doc.get("varcap")),str(info.get("via")),str(info.get("varcitta")),str(info.get("varprovincia")),str(info.get("varpaese")),str(info.get("varcap")))
 				if distanza <= float(info.get("tolleranza")):
-					print("[algoritmo]",distanza,"e' minore dellatolleranza  "+ info.get("tolleranza"))
+					#print("[algoritmo]",distanza,"e' minore dellatolleranza  "+ info.get("tolleranza"))
 					lista_serie.append(elem)
 				else :
 					print("[algoritmo]",distanza,"e' maggiore della tolleranza "+ info.get("tolleranza"))
@@ -129,7 +129,8 @@ fopen = open("parte_fissa.txt","r")
 @app.route('/search', methods = ["POST"])
 def search():
 	parte_fissa = str(fopen.read())
-	print(parte_fissa)
+
+	#print(parte_fissa)
 	if request.method == "POST":
 		#varindirizzo = request.form["via"]
 		#varcitta = req.form["varcitta"]
@@ -146,12 +147,17 @@ def search():
 			ris = json.loads(json_ris)
 			for i in range(0,int(json.loads(json_ris)["numero"])):
 				stampante = ris[str(i)]
-				print(stampante)
-				testo="<div role='tabpanel' class='description'><div class='tab-pane active'><div class='col-xs-12'>"+"<h1><center>Risultato numero: "+str(i+1)+"<center><br></h1>"+"<h2>"+"venditore"+stampante["varuser"]+"<br></h2>"+"<p>"+"indirizzo: "+stampante["varindirizzo"]+"<br>"+"citta: "+stampante["varcitta"]+"<br>"+"email: "+stampante["varemail"]+"<br>"+"telefono: "+stampante["vartelefono"]+"<br>"""+"tipo stampante: "+stampante["stampantetipo"]+"<br>"+"nome stampante: "+stampante["stampantenome"]+"<br>"+"id stampante: "+stampante["stampanteid"]+"<br>"""+"prezzo stampante: "+stampante["stampanteprezzo"]+"<br></p></div>"+"</div></div></body></head>"
-				parte_fissa+=testo
+				#print(stampante)
+				
+				# testo="<div role='tabpanel' class='description'><div class='tab-pane active'><div class='col-xs-12'>"+"<h1><center>Risultato numero: "+str(i+1)+"<center><br></h1>"+"<h2>"+"venditore"+stampante["varuser"]+"<br></h2>"+"<p>"+"indirizzo: "+stampante["varindirizzo"]+"<br>"+"citta: "+stampante["varcitta"]+"<br>"+"email: "+stampante["varemail"]+"<br>"+"telefono: "+stampante["vartelefono"]+"<br>"""+"tipo stampante: "+stampante["stampantetipo"]+"<br>"+"nome stampante: "+stampante["stampantenome"]+"<br>"+"id stampante: "+stampante["stampanteid"]+"<br>"""+"prezzo stampante: "+stampante["stampanteprezzo"]+"<br></p></div>"+"</div></div>"
+				testo_leo="<tr><td>"+stampante["varuser"]+"</td><td>"+stampante["varindirizzo"]+"</td><td>"+stampante["varcitta"]+"</td><td>"+stampante["varemail"]+"</td><td>"+stampante["vartelefono"]+"</td><td>"+stampante["stampantetipo"]+"</td><td>"+stampante["stampantenome"]+"</td><td>"+stampante["stampanteid"]+"</td><td>"+stampante["stampanteprezzo"]+"</td><td><button id= "+str(i+1)+">Contatta</button></td></tr>"
+
+				parte_fissa+=testo_leo
+			parte_fissa+="</table></body></html>"
+			print(parte_fissa)
 			return parte_fissa
 			
 		else:
-			return parte_fissa+"<div role='tabpanel' class='description'><div class='tab-pane active'>"+"<h1><center>Nessun risultato,siamo spiacenti!<center></h1></div></div></body></head>"
+			return parte_fissa+"<div role='tabpanel' class='description'><div class='tab-pane active'>"+"<h1><center>Nessun risultato,siamo spiacenti!<center></h1></div></div></body></html>"
 		
 	
